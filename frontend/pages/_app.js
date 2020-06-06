5a057eec2db241ebd6055ff4923f0e581c0c12cb
yuanxindong <yuanxin.me@gmail.com>
2020-06-06 15:34:57 +0800

    init: frontend

---
+++ frontend/pages/_app.js
@@ -0,0 +1,9 @@
+import 'antd/dist/antd.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+    
+    return <>
+        <Component {...pageProps} />
+    </>
+}
