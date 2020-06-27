8fa4c41b84a46c8a30896e628dcb8ec5cddddc1f
dongyuanxin <yuanxin.me@gmail.com>
2020-06-27 22:41:18 +0800

    feat(component): 动态加载icon，防止svg渲染到html模板

---
+++ frontend/components/Icon/index.jsx
@@ -0,0 +1,51 @@
+import dynamic from 'next/dynamic'
+
+export const CoffeeOutlined = dynamic(
+    () => import('@ant-design/icons/CoffeeOutlined'),
+    { ssr: false }
+)
+
+export const AppstoreOutlined = dynamic(
+    () => import('@ant-design/icons/AppstoreOutlined'),
+    { ssr: false }
+)
+
+export const CommentOutlined = dynamic(
+    () => import('@ant-design/icons/CommentOutlined'),
+    { ssr: false }
+)
+
+export const FireOutlined = dynamic(
+    () => import('@ant-design/icons/FireOutlined'),
+    { ssr: false }
+)
+
+export const BellOutlined = dynamic(
+    () => import('@ant-design/icons/BellOutlined'),
+    { ssr: false }
+)
+
+export const GithubOutlined = dynamic(
+    () => import('@ant-design/icons/GithubOutlined'),
+    { ssr: false }
+)
+
+export const SearchOutlined = dynamic(
+    () => import('@ant-design/icons/SearchOutlined'),
+    { ssr: false }
+)
+
+export const ClockCircleOutlined = dynamic(
+    () => import('@ant-design/icons/ClockCircleOutlined'),
+    { ssr: false }
+)
+
+export const EyeOutlined = dynamic(
+    () => import('@ant-design/icons/EyeOutlined'),
+    { ssr: false }
+)
+
+export const MessageOutlined = dynamic(
+    () => import('@ant-design/icons/MessageOutlined'),
+    { ssr: false }
+)
