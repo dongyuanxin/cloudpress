789be0d904f98f1baa1ea606aec9e19cedc91907
dongyuanxin <yuanxin.me@gmail.com>
2020-07-01 00:38:30 +0800

    feat: 构建时去掉敏感信息

--- frontend/pages/[psgID].jsx
+++ frontend/pages/[psgID].jsx
@@ -2,12 +2,19 @@ import React, { useEffect } from 'react'
 import { md } from '../helpers/markdown' 
 import _ from 'lodash'
 import * as prettier from 'prettier'
+import { Row, Col } from 'antd' 
+import { EyeOutlined, MessageOutlined } from './../components/Icon'
 import { describePsgIDs, describePassage } from './../providers/passage'
 
-const BlogPage = ({ contentHtml }) => {
-    const a = 1
+const BlogPage = ({ contentHtml, passage }) => {
     return (
         <div className="page-article">
+            <h1>{passage.title}</h1>
+            {/* <Row gutter={8}>
+                <Col span={8}>
+                    发布于 { passage.publishTime }
+                </Col>
+            </Row> */}
             <div className="markdown" dangerouslySetInnerHTML={{__html: contentHtml}}></div>
         </div>
     )
@@ -31,12 +38,12 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
     const passage = await describePassage(params.psgID)
-    const content = prettier.format(`# ${passage.title}\n` + passage.content, { parser: 'markdown' })
+    const content = prettier.format(passage.content, { parser: 'markdown' })
 
     return {
         props: {
             contentHtml: md.render(content),
-            passage: _.omit(passage, ['updateTime', 'createTime'])
+            passage: _.omit(passage, ['updateTime', 'createTime', 'content', 'id', '_id'])
         }
     }
 }
