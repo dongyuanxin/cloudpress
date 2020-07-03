aecdecb5115621f81091305a9b2510b977734e21
yuanxindong <yuanxin.me@gmail.com>
2020-07-03 13:52:46 +0800

    style(mobile): set viewport width on mobile

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { Layout, BackTop } from "antd";
 import CustomFooter from "./../components/CustomFooter/";
 import Navigation from "./../components/Navigation/";
+import Head from "next/head";
 import "./../styles/index.scss";
 import "highlight.js/styles/atom-one-light.css";
 
@@ -23,13 +24,20 @@ if (process.browser) {
 export default function MyApp({ Component, pageProps }) {
     return (
         <Layout style={{ background: "white" }}>
+            {/* todo: 移动端兼容 */}
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=1024, user-scalable=yes, initial-scale=1"
+                />
+            </Head>
             <Navigation style={{ background: "white" }} />
             <Content
                 style={{
                     width: "1040px",
                     maxWidth: "95vw",
                     margin: "0 auto",
-                    minHeight: "calc(100vh - 64px - 340px)",
+                    minHeight: "calc(100vh - 64px - 307px)",
                 }}
             >
                 <Component {...pageProps} />
