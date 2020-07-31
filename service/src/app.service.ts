3d7014a6b6062caefd353c0d42301885a691f344
yuanxindong <yuanxin.me@gmail.com>
2020-07-31 17:41:39 +0800

    feat(scripts): support docker zip scripts

--- service/src/app.service.ts
+++ service/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Hello World!';
+        return 'Hello Cloudpress! View demo at: https://xxoo521.com/';
     }
 }
