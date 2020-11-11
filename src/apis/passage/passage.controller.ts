be3c367b9184d9e2bc8a519e689646136287d897
dongyuanxin <yuanxin.me@gmail.com>
2020-11-12 00:27:34 +0800

    feat(passage): 支持passage相关接口
    feat(passage): 支持passage相关接口

--- src/apis/passage/passage.controller.ts
+++ src/apis/passage/passage.controller.ts
@@ -3,13 +3,41 @@ import {
     Get,
     Query,
     ValidationPipe,
-    UsePipes,
 } from '@nestjs/common';
 import { PassageService } from './passage.service';
+import { DescribePassageByIdDto, DescribePassagesDto } from './passage.dto';
 
-@Controller('notice')
+@Controller('passage')
 export class PassageController {
     constructor(private readonly passageService: PassageService) {
         this.passageService.load()
+    }
+
+    @Get('describe-passage-by-id')
+    async describePassageById(
+        @Query(new ValidationPipe({ whitelist: false }))
+        query: DescribePassageByIdDto
+    ) {
+        const { passageId } = query
+        return await this.passageService.describePassageById(passageId);
+    }
+
+    @Get('describe-passages')
+    async describePassages(
+        @Query(new ValidationPipe({ whitelist: true, transform: true }))
+        query: DescribePassagesDto
+    ) {
+        const { limit, page } = query
+        return await this.passageService.describePassages(limit, page)
+    }
+
+    @Get('count-all-passages')
+    async countAllPassages() {
+        return await this.passageService.countAllPassages()
+    }
+
+    @Get('describe-all-passage-ids')
+    async describeAllPassageIds() {
+        return await this.passageService.describePassages()
     }
 }
